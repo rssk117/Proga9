dada48877381c9e5d014ced98257975dcd101309
попр <@OMEGA001>
2022-09-28 12:19:14 +0300

    2222

--- Пр№9/Baggage.cs
+++ Пр№9/Baggage.cs
@@ -13,7 +13,7 @@ namespace Пр_9
         public Baggage(double _totalWeight, int _quantityThings)
         {
             TotalWeight =_totalWeight;
-        
+            TotalWeight = _totalWeight;
             QuantityThings = _quantityThings;
             
         }
